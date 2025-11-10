87dacbccb7f7e862201a4d085d9311a924a3eb39
sergeysepkhanyan <sepkhanyan.sergey@gmail.com>
2025-11-10 14:10:22 +0400

    add products

--- app/Http/Controllers/API/Admin/ProductsController.php
+++ app/Http/Controllers/API/Admin/ProductsController.php
@@ -21,44 +21,44 @@ class ProductsController extends Controller
 
     public function store(Request $request): JsonResponse
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'description' => 'nullable|string',
-            'max_quantity' => 'nullable|integer',
-            'price' => 'required|numeric',
-            'currency' => 'required|string|max:10',
-            'main_image' => 'nullable|string',
-            'referral_id' => 'nullable|integer',
-            'discount' => 'nullable|boolean',
-            'discount_type' => 'nullable|string|in:percentage,amount',
-            'discount_amount' => 'nullable|numeric',
-            'status' => 'nullable|in:active,draft',
-
-            'images' => 'required|array|min:1',
-            'images.*' => 'string',
-
-            'details' => 'nullable|array',
-            'details.*.details' => 'required|string',
-            'details.*.description' => 'nullable|string',
-        ], [
-            'images.required' => 'At least one file path is required for the product.',
-            'images.min' => 'At least one file path is required for the product.',
-        ]);
-
-        if ($validator->fails()) {
-            return ApiResponse::error($validator->errors(), 'Validation failed', 422);
-        }
-
-        $productData = $request->only([
-            'name', 'description', 'max_quantity', 'price', 'currency',
-            'main_image', 'referral_id', 'discount', 'discount_type',
-            'discount_amount', 'status'
-        ]);
-
-        $productFilePaths = $request->input('images', []);
-        $detailsData = $request->input('details', []);
-        $productData['main_image'] = $productFilePaths[0];
         try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:255',
+                'description' => 'nullable|string',
+                'max_quantity' => 'nullable|integer',
+                'price' => 'required|numeric',
+                'currency' => 'required|string|max:10',
+                'main_image' => 'nullable|string',
+                'referral_id' => 'nullable|integer',
+                'discount' => 'nullable|boolean',
+                'discount_type' => 'nullable|string|in:percentage,amount',
+                'discount_amount' => 'nullable|numeric',
+                'status' => 'nullable|in:active,draft',
+
+                'images' => 'required|array|min:1',
+                'images.*' => 'string',
+
+                'details' => 'nullable|array',
+                'details.*.details' => 'required|string',
+                'details.*.description' => 'nullable|string',
+            ], [
+                'images.required' => 'At least one file path is required for the product.',
+                'images.min' => 'At least one file path is required for the product.',
+            ]);
+
+            if ($validator->fails()) {
+                return ApiResponse::error($validator->errors(), 'Validation failed', 422);
+            }
+
+            $productData = $request->only([
+                'name', 'description', 'max_quantity', 'price', 'currency',
+                'main_image', 'referral_id', 'discount', 'discount_type',
+                'discount_amount', 'status'
+            ]);
+
+            $productFilePaths = $request->input('images', []);
+            $detailsData = $request->input('details', []);
+            $productData['main_image'] = $productFilePaths[0];
             $product = $this->productService->createProduct($productData, $detailsData, $productFilePaths);
             return ApiResponse::success([
                 'product' => new ProductResource($product),
@@ -70,51 +70,50 @@ class ProductsController extends Controller
 
     public function update(Request $request, $id): JsonResponse
     {
-        $product = $this->productService->getProductById($id);
-
-        if (!$product) {
-            return ApiResponse::error([], 'Product not found', 404);
-        }
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'description' => 'nullable|string',
-            'max_quantity' => 'nullable|integer',
-            'price' => 'required|numeric',
-            'currency' => 'required|string|max:10',
-            'main_image' => 'nullable|string',
-            'referral_id' => 'nullable|integer',
-            'discount' => 'nullable|boolean',
-            'discount_type' => 'nullable|string|in:percentage,amount',
-            'discount_amount' => 'nullable|numeric',
-            'status' => 'nullable|in:active,draft',
-
-            'removed_files' => 'nullable|array',
-            'removed_files.*' => 'string',
-
-            'new_files' => 'nullable|array',
-            'new_files.*' => 'string',
-
-            'details' => 'nullable|array',
-            'details.*.id' => 'nullable|integer',
-            'details.*.details' => 'required|string',
-            'details.*.description' => 'nullable|string',
-        ]);
-
-        if ($validator->fails()) {
-            return ApiResponse::error($validator->errors(), 'Validation failed', 422);
-        }
-
-        $productData = $request->only([
-            'name', 'description', 'max_quantity', 'price', 'currency',
-            'main_image', 'referral_id', 'discount', 'discount_type',
-            'discount_amount', 'status'
-        ]);
-
-        $removedFiles = $request->input('removed_files', []);
-        $newFiles = $request->input('images', []);
-        $detailsData = $request->input('details', []);
-
         try {
+            $product = $this->productService->getProductById($id);
+
+            if (!$product) {
+                return ApiResponse::error([], 'Product not found', 404);
+            }
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:255',
+                'description' => 'nullable|string',
+                'max_quantity' => 'nullable|integer',
+                'price' => 'required|numeric',
+                'currency' => 'required|string|max:10',
+                'main_image' => 'nullable|string',
+                'referral_id' => 'nullable|integer',
+                'discount' => 'nullable|boolean',
+                'discount_type' => 'nullable|string|in:percentage,amount',
+                'discount_amount' => 'nullable|numeric',
+                'status' => 'nullable|in:active,draft',
+
+                'removed_files' => 'nullable|array',
+                'removed_files.*' => 'string',
+
+                'new_files' => 'nullable|array',
+                'new_files.*' => 'string',
+
+                'details' => 'nullable|array',
+                'details.*.id' => 'nullable|integer',
+                'details.*.details' => 'required|string',
+                'details.*.description' => 'nullable|string',
+            ]);
+
+            if ($validator->fails()) {
+                return ApiResponse::error($validator->errors(), 'Validation failed', 422);
+            }
+
+            $productData = $request->only([
+                'name', 'description', 'max_quantity', 'price', 'currency',
+                'main_image', 'referral_id', 'discount', 'discount_type',
+                'discount_amount', 'status'
+            ]);
+
+            $removedFiles = $request->input('removed_files', []);
+            $newFiles = $request->input('images', []);
+            $detailsData = $request->input('details', []);
             $product = $this->productService->updateProduct(
                 $id,
                 $productData,
