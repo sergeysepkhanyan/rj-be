f3178995988d605794664b510ff6dfe11d9165ed
sergeysepkhanyan <sepkhanyan.sergey@gmail.com>
2025-11-14 12:23:09 +0400

    new changes

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,14 +8,22 @@ use Symfony\Component\HttpFoundation\Response;
 
 class Cors
 {
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
 
-        return $response
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS', 'PATCH')
-            ->header('Access-Control-Allow-Headers', '*');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', '*');
+        $response->headers->set('Access-Control-Allow-Headers', '*');
+
+        if ($request->getMethod() === "OPTIONS") {
+            return response('', 200)
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', '*')
+                ->header('Access-Control-Allow-Headers', '*');
+        }
+
+        return $response;
     }
 
 }
