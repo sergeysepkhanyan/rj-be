73926e2909343ce4e1acf7092d47bf8f1501ed5e
sergeysepkhanyan <sepkhanyan.sergey@gmail.com>
2025-12-10 01:05:15 +0400

    create booking

---
+++ app/Http/Resources/AdminServiceResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property mixed $name
+ * @property mixed $description
+ * @property mixed $image
+ * @property mixed $subServices
+ * @property mixed $id
+ */
+class AdminServiceResource extends BaseResource
+{
+    public function toArray($request): array
+    {
+        $data = parent::toArray($request);
+        return [
+            'id' => $data['id'] ?? null,
+            'name' => $data['name'] ?? null,
+            'description' => $data['description'] ?? null,
+            'image' => $this->image ? asset('storage/' . $this->image) : null,
+            'subservices' => SubServiceResource::collection($this->subServices),
+        ];
+    }
+}
+
