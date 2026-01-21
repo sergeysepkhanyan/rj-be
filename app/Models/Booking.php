3ceede768b355b236dc761e26120de4636c662d1
sergeysepkhanyan <sepkhanyan.sergey@gmail.com>
2026-01-21 23:26:11 +0400

    new changes

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -11,6 +11,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
