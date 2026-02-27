8287984b13dfadd436234ee468df3a76cffe557d
Arthur Tadevosyan <arthur.tadevosyan.dev@gmail.com>
2026-02-27 13:14:13 +0400

    Fix: change validation to expect homepage instead of home

--- app/Http/Requests/UpdatePagesRequest.php
+++ app/Http/Requests/UpdatePagesRequest.php
@@ -11,12 +11,12 @@ class UpdatePagesRequest extends BaseFormRequest
         $others = 'about,contact,blog,store,general';
 
         return [
-            'home' => ["required_without_all:{$others}", 'array'],
-            'about' => ['required_without_all:home,contact,blog,store,general', 'array'],
-            'contact' => ['required_without_all:home,about,blog,store,general', 'array'],
-            'blog' => ['required_without_all:home,about,contact,store,general', 'array'],
-            'store' => ['required_without_all:home,about,contact,blog,general', 'array'],
-            'general' => ['required_without_all:home,about,contact,blog,store', 'array'],
+            'homepage' => ["required_without_all:{$others}", 'array'],
+            'about' => ['required_without_all:homepage,contact,blog,store,general', 'array'],
+            'contact' => ['required_without_all:homepage,about,blog,store,general', 'array'],
+            'blog' => ['required_without_all:homepage,about,contact,store,general', 'array'],
+            'store' => ['required_without_all:homepage,about,contact,blog,general', 'array'],
+            'general' => ['required_without_all:homepage,about,contact,blog,store', 'array'],
         ];
     }
 }
